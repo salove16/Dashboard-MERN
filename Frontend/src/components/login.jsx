266cff2c00c0a7d0ac91aa81fea636b90d63803e
salove16 <shashank.shalabh909@gmail.com>
2022-07-22 22:55:06 +0530

    login and sign up bug fixed

--- Frontend/src/components/login.jsx
+++ Frontend/src/components/login.jsx
@@ -1,64 +1,114 @@
 import {
-    Avatar,
-    Button,
-    CssBaseline,
-    TextField,
-    Grid,
-    Box,
-    Typography,
-    Container,
-    Paper,
-    InputAdornment,
-    IconButton,
-  } from "@mui/material";
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  Grid,
+  Box,
+  Typography,
+  Container,
+  Paper,
+} from "@mui/material";
 
-  import {Link} from "react-router-dom" 
-  export const Login = () => {
-  
-  
-  
-  
-    return (
-      <Container component="main" sx={{m:5,mx:"auto"}} maxWidth="sm">
-          <Avatar sx={{ width: 100, height: 100 ,m:5, mx:"auto"  }} >SS</Avatar>
-        <Box component="form">
-          <Grid container spacing={2}>
-            
-            <Grid item xs={12} sm={12}>
-              <TextField
-                required
-                fullWidth
-                id="Email"
-                label="Email"
-                name="Email"
-                type="email"
-                // autoComplete="family-name"
-              />
-            </Grid>
-            <Grid item xs={12} sm={12}>
-              <TextField
-                required
-                fullWidth
-                id="name"
-                label="password"
-                name="password"
-                type="password"
-                
-              /> 
-              
-              
-             </Grid> 
-          
-            <Grid item xs={12} sm={12} display="flex" justifyContent="space-between">
-              <Button variant="contained" type="submit" sx={{}}>Login</Button>
-        
-              <Typography >
-              Dont't have an Account? <Link to="/">signup</Link>
-              </Typography>
-            </Grid>
-          </Grid>
-        </Box>
-      </Container>
-    );
+import { useState } from "react";
+
+import { Link,useNavigate } from "react-router-dom";
+
+
+
+
+export const Login = () => {
+  const [text, setText] = useState();
+  const navigate=useNavigate()
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // console.log(name,value)
+    setText({ ...text, [name]: value });
   };
-  
+  // console.log(text)
+
+  const loginReq = async (e) => {
+    try {
+        e.preventDefault()
+        // console.log(text)
+      const { Email, password } = text;
+      let payload;
+      if (Email && password) {
+        payload = {
+          email: Email,
+          password: password,
+        };
+      }
+      console.log(payload);
+
+      let res = await fetch("http://localhost:4000/login", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+    //   console.log(res)
+      let data = await res.json();
+      navigate("/dashboard");
+    } catch (error) {
+      console.log({ message: error.message });
+    }
+  };
+
+  return (
+    <Container component="main" sx={{ m: 5, mx: "auto" }} maxWidth="sm">
+      <Avatar sx={{ width: 100, height: 100, m: 5, mx: "auto" }}>SS</Avatar>
+      <Box component="form">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={12}>
+            <TextField
+              required
+              fullWidth
+              id="Email"
+              label="Email"
+              name="Email"
+              type="email"
+              // autoComplete="family-name"
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12}>
+            <TextField
+              required
+              fullWidth
+              id="name"
+              label="password"
+              name="password"
+              type="password"
+              onChange={handleChange}
+            />
+          </Grid>
+
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            display="flex"
+            justifyContent="space-between"
+          >
+            <Button
+              variant="contained"
+              type="submit"
+              sx={{}}
+              onClick={loginReq}
+            >
+              Login
+            </Button>
+
+            <Typography>
+              Dont't have an Account? <Link to="/">signup</Link>
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
+};
--- Frontend/src/components/signup.jsx
+++ Frontend/src/components/signup.jsx
@@ -11,9 +11,51 @@ import {
   InputAdornment,
   IconButton,
 } from "@mui/material";
+import { useState } from "react";
 
-import {Link} from "react-router-dom"
+
+import {Link,useNavigate} from "react-router-dom"
 export const SignUp = () => {
+    const [text, setText] = useState();
+  const navigate=useNavigate()
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // console.log(name,value)
+    setText({ ...text, [name]: value });
+  };
+  // console.log(text)
+
+  const signupReq = async (e) => {
+    try {
+        e.preventDefault()
+        // console.log(text)
+      const { name,Email, password } = text;
+      let payload;
+      if (name && Email && password) {
+        payload = {
+            userName:name,
+          email: Email,
+          password: password,
+        };
+      }
+      console.log(payload);
+
+      let res = await fetch("http://localhost:4000/signup", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+    //   console.log(res)
+      let data = await res.json();
+      navigate("/login");
+    } catch (error) {
+      console.log({ message: error.message });
+    }
+  };
 
 
 
@@ -30,6 +72,7 @@ export const SignUp = () => {
               id="name"
               label="name"
               name="name"
+              onChange={handleChange}
               // autoComplete="family-name"
             />
           </Grid>
@@ -41,6 +84,7 @@ export const SignUp = () => {
               label="Email"
               name="Email"
               type="email"
+              onChange={handleChange}
               // autoComplete="family-name"
             />
           </Grid>
@@ -52,6 +96,7 @@ export const SignUp = () => {
               label="password"
               name="password"
               type="password"
+              onChange={handleChange}
               
             /> 
             
@@ -59,7 +104,7 @@ export const SignUp = () => {
            </Grid> 
         
           <Grid item xs={12} sm={12} display="flex" justifyContent="space-between">
-            <Button variant="contained" type="submit" sx={{}}>SignUp</Button>
+            <Button variant="contained" type="submit" sx={{}}   onClick={signupReq}>SignUp</Button>
       
             <Typography >
             Already have an Account? <Link to="/login">login</Link>
